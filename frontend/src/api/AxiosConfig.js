0115856ee3f2fbc60db145eec296ca6925e6bdba
lukas <l.krzywda92@gmail.com>
2022-04-04 00:15:39 +0200

    config files

--- frontend/src/api/AxiosConfig.js
+++ frontend/src/api/AxiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://127.0.0.1:8000'
+const baseURL = 'https://satellite-app-lk.herokuapp.com'
 
 let token = localStorage.getItem('token') ? JSON.parse(localStorage.getItem('token')) : null
 
